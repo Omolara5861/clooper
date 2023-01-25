8e30b6c4d8e3c59e464449c194fa73fe0c1bf29b
Omolara5861 <debbiegterra@gmail.com>
2023-01-25 22:18:00 +0100

    feat: :sparkles: Update accordion component logic

--- projects/clooper-common/src/lib/components/accordion/accordion.component.ts
+++ projects/clooper-common/src/lib/components/accordion/accordion.component.ts
@@ -6,10 +6,7 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./accordion.component.css']
 })
 export class AccordionComponent {
-
   @Input() headerText!: string;
   @Input() bodyText!: string;
-  @Input() accordionColor!: string;
-
-  isAccordionOpen = true;
+  @Input() id!: string;
 }
