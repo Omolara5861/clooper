8c352f1d35d9a214280a157f122bea0c0a07284c
Omolara5861 <debbiegterra@gmail.com>
2023-01-25 22:34:43 +0100

    feat: :sparkles: Add users service

--- projects/clooper-core/src/lib/services/users.service.ts
+++ projects/clooper-core/src/lib/services/users.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
 
+export interface User {
+  name: string;
+  email: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
   constructor() { }
+
+  getUsers(users: User[]) {
+    return users;
+  }
 }
