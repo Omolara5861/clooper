51e93919cf10976f27075e6710836e53d5cea946
Omolara5861 <debbiegterra@gmail.com>
2023-01-25 14:28:00 +0100

    chore: :fire: Remove missing exported files

--- projects/clooper-common/src/public-api.ts
+++ projects/clooper-common/src/public-api.ts
@@ -1,7 +1,4 @@
 /*
  * Public API Surface of clooper-common
  */
-
-export * from './lib/clooper-common.service';
-export * from './lib/clooper-common.component';
 export * from './lib/clooper-common.module';
