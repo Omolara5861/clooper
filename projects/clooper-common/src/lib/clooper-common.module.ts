9a836558e3cb3659bae1bcf933d98729d8cd0fac
Omolara Adebowale <debbiegterra@gmail.com>
2023-01-25 16:55:09 +0100

    refactor: :art: Format module file

--- projects/clooper-common/src/lib/clooper-common.module.ts
+++ projects/clooper-common/src/lib/clooper-common.module.ts
@@ -2,20 +2,14 @@ import { NgModule } from '@angular/core';
 import { PaginationComponent } from './components/pagination/pagination.component';
 import { ButtonsComponent } from './components/buttons/buttons.component';
 import { AccordionComponent } from './components/accordion/accordion.component';
-import { FullnamePipe } from './pipes/fullname.pipe';
 import { FullNamePipe } from './pipes/full-name.pipe';
-
-
-
 
 @NgModule({
   declarations: [
-  
     PaginationComponent,
-       ButtonsComponent,
-       AccordionComponent,
-       FullnamePipe,
-       FullNamePipe
+    ButtonsComponent,
+    AccordionComponent,
+    FullNamePipe
   ],
   imports: [
   ],
