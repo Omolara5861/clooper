528697d55156e5af49ef404b215f36d4be3c4908
Omolara Adebowale <debbiegterra@gmail.com>
2023-01-25 11:50:16 +0100

    refactor: :fire: Delete all unused file in common lib

--- projects/clooper-common/src/lib/clooper-common.module.ts
+++ projects/clooper-common/src/lib/clooper-common.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
-import { ClooperCommonComponent } from './clooper-common.component';
 
 
 
 @NgModule({
   declarations: [
-    ClooperCommonComponent
   ],
   imports: [
   ],
   exports: [
-    ClooperCommonComponent
   ]
 })
 export class ClooperCommonModule { }
