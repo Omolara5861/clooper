599f7035ee77a2a137730217f5bd04cd400ad138
Omolara Adebowale <debbiegterra@gmail.com>
2023-01-25 16:16:32 +0100

    feat:

--- projects/clooper-common/src/lib/components/buttons/buttons.component.ts
+++ projects/clooper-common/src/lib/components/buttons/buttons.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'lib-buttons',
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class ButtonsComponent {
 
+  @Input() label!: string;
+  @Input() btnColor!: string;
+  @Input() message!: string;
+
+  @Output() onClick = new EventEmitter<string>();
+
+
+  buttonClicked() {
+    this.onClick.emit(this.message)
+  }
 }
